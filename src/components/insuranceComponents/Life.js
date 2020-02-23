51092f6303290916fa78f8c80df3a61c75e0fedd
Ruben Verster <ruben.verster@younglings.africa>
2020-02-23 12:35:36 +0200

    trust page setup, life page scrapped, woring on Trust Form Creation

--- src/components/insuranceComponents/Life.js
+++ src/components/insuranceComponents/Life.js
@@ -1,136 +1,37 @@
-import React, { useState, setState, Component } from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import firebase from 'firebase';
-// import { LifeCrud } from '../CRUD/Crud';
-import {
-  Row,
-  Card,
-  Col,
-  Button,
-  UncontrolledCollapse,
-  CardBody
-} from 'reactstrap';
-import InsuranceActionInput from '../CRUD/InsuranceActionInput';
+import { Container, Row, Col } from 'reactstrap';
 
-const LifeActionCreate = () => {
-  let [newLifeActionLabel, setNewLifeActionLabel] = React.useState();
-
-  const onCreate = e => {
-    e.preventDefault();
-
-    const db = firebase.firestore();
-
-    //why does ternary operator not work.......?
-    // (newLifeActionLabel !== '') ? (
-    //   // db.collection('actions').add({
-    //   //   label: newLifeActionLabel
-    //   // });
-    //   alert('New Life Insurance Added');
-    //   setNewLifeActionLabel('');
-    // ) : (
-    //   alert('Please Review Your Data');
-    // )
-
-    if (newLifeActionLabel !== '') {
-      // db.collection('actions').add({
-      //   label: newLifeActionLabel
-      // });
-      alert('New Life Insurance Added');
-      setNewLifeActionLabel('');
-    } else {
-      alert('Please Review Your Data');
-    }
-  };
-
-  return (
-    <form onSubmit={onCreate}>
-      <label>Name</label>
-      <input
-        value={newLifeActionLabel}
-        onChange={e => {
-          setNewLifeActionLabel(e.target.value);
-        }}
-        placeholder={'Name'}
-      />
-      <Button onClick={onCreate}>Create</Button>
-    </form>
-  );
-};
-
-const LifeActionRead = (props, { LifeAction }) => {
+const Life = props => {
   //check permissions in firestore, read and write access granted?
 
-  const [lifeActions, setLifeActions] = React.useState([]);
+  // const [medActions, setPets] = React.useState([]);
 
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const db = firebase.firestore();
-      const data = await db.collection('actions').get();
-      setLifeActions(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
-    };
-    fetchData();
-  }, []);
+  // React.useEffect(() => {
+  //   const fetchData = async () => {
+  //     const db = firebase.firestore();
+  //     const data = await db.collection('actions').get();
+  //     setPets(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+  //   };
+
+  //   fetchData();
+  // }, []);
 
   return (
-    <Col style={{ marginTop: 30 }}>
-      {props.actionType === 'new-medical-aid' ? (
-        <Row>
-          <div>Hi</div>
-          <div>{props.label}</div>
-        </Row>
-      ) : (
-        {}
-      )}
-    </Col>
-  );
-};
-
-const Life = () => {
-  //check permissions in firestore, read and write access granted?
-
-  const [lifeActions, setLifeActions] = React.useState([]);
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const db = firebase.firestore();
-      const data = await db.collection('actions').get();
-      setLifeActions(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
-    };
-    fetchData();
-  }, []);
-  return (
-    <div>
-      <Row>
-        <Button color="danger" id="toggler" style={{ marginBottom: '1rem' }}>
-          <strong>+</strong> Add New Life Insurance
-        </Button>
-        <UncontrolledCollapse toggler="#toggler">
-          <Card>
-            <CardBody>
-              <LifeActionCreate />
-            </CardBody>
-          </Card>
-        </UncontrolledCollapse>
-      </Row>
-      <Row style={{ marginTop: 30 }}>
-        <div>
-          {lifeActions.map(action => (
-            <div>
-              {action.actionType === 'new-medical-aid' ? (
-                <Card>
-                  <span>{action.label}</span>
-                  <InsuranceActionInput
-                    lifeAction={action}
-                  ></InsuranceActionInput>
-                </Card>
-              ) : (
-                ''
-              )}
-            </div>
-          ))}
-        </div>
-      </Row>
-    </div>
+    <Container>
+      {/* <Row style={{ marginTop: 30 }}>
+        <Col>
+          <div>
+            {medActions.map(action => (
+              <h1 medAction={action}></h1>
+            ))}
+          </div>
+        </Col>
+        <Col>Hi</Col>
+      </Row> */}
+      <p>Life Page</p>
+    </Container>
   );
 };
 
--- src/components/insuranceComponents/Trust.js
+++ src/components/insuranceComponents/Trust.js
@@ -1,40 +1,213 @@
-import React, { useState } from 'react';
+import React, { useState, setState, Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import firebase from 'firebase';
-import { Container, Row, Col } from 'reactstrap';
-import { TrustCrud } from '../CRUD/Crud';
+import {
+  Row,
+  Card,
+  Col,
+  Button,
+  UncontrolledCollapse,
+  CardBody
+} from 'reactstrap';
 
-const Trust = props => {
-  //check permissions in firestore, read and write access granted?
+const UnitTrustActionCreate = () => {
+  let [newUnitTrustActionType, setNewUnitTrustActionType] = React.useState();
+  let [
+    newUnitTrustDebitOrderAmount,
+    setNewUnitTrustDebitOrderAmount
+  ] = React.useState();
+  let [
+    newUnitTrustDebitOrderDay,
+    setNewUnitTrustDebitOrderDay
+  ] = React.useState();
+  let [newUnitTrustFundName, setNewFundName] = React.useState();
+  let [newUnitTrustActionLabel, setNewUnitTrustActionLabel] = React.useState();
+  let [newUnitTrustLumpSum, setNewUnitTrustLumoSum] = React.useState();
+  let [newUnitTrustStartMonth, setNewUnitTrustStartMonth] = React.useState();
 
-  // const [medActions, setPets] = React.useState([]);
+  const onCreate = e => {
+    e.preventDefault();
 
-  // React.useEffect(() => {
-  //   const fetchData = async () => {
-  //     const db = firebase.firestore();
-  //     const data = await db.collection('actions').get();
-  //     setPets(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
-  //   };
+    const db = firebase.firestore();
 
-  //   fetchData();
-  // }, []);
+    //why does ternary operator not work.......?
+    // (newUnitTrustActionLabel !== '') ? (
+    //   // db.collection('actions').add({
+    //   //   label: newUnitTrustActionLabel
+    //   // });
+    //   alert('New UnitTrust Insurance Added');
+    //   setNewUnitTrustActionLabel('');
+    // ) : (
+    //   alert('Please Review Your Data');
+    // )
+
+    if (newUnitTrustActionLabel !== '') {
+      // db.collection('actions').add({
+      //   label: newUnitTrustActionLabel
+      // });
+      alert('New UnitTrust Insurance Added');
+      setNewUnitTrustActionLabel('');
+    } else {
+      alert('Please Review Your Data');
+    }
+  };
 
   return (
-    <Container>
-      {/* <Row style={{ marginTop: 30 }}>
-        <Col>
-          <div>
-            {medActions.map(action => (
-              <h1 medAction={action}></h1>
-            ))}
-          </div>
-        </Col>
-        <Col>Hi</Col>
-      </Row> */}
-      <p>TRUST Page</p>
-      <TrustCrud />
-    </Container>
+    <form onSubmit={onCreate}>
+      <label>Debit Order</label>
+      <div>
+        <input
+          value={newUnitTrustActionLabel}
+          onChange={e => {
+            setNewUnitTrustActionLabel(e.target.value);
+          }}
+          placeholder={'Debit Order Amount'}
+        />
+        <input
+          value={newUnitTrustActionLabel}
+          onChange={e => {
+            setNewUnitTrustActionLabel(e.target.value);
+          }}
+          placeholder={'Debit Order Day'}
+        />
+      </div>
+
+      <label>Fund Name</label>
+      <input
+        value={newUnitTrustActionLabel}
+        onChange={e => {
+          setNewUnitTrustActionLabel(e.target.value);
+        }}
+        placeholder={'Fund Name'}
+      />
+
+      <label>Label</label>
+      <input
+        value={newUnitTrustActionLabel}
+        onChange={e => {
+          setNewUnitTrustActionLabel(e.target.value);
+        }}
+        placeholder={'Open A New Unit Trust'}
+      />
+
+      <label>Lump Sum Contribution</label>
+      <input
+        value={newUnitTrustActionLabel}
+        onChange={e => {
+          setNewUnitTrustActionLabel(e.target.value);
+        }}
+        placeholder={'Lump Sum Contribution'}
+      />
+
+      <label>Start Month</label>
+      <input
+        value={newUnitTrustActionLabel}
+        onChange={e => {
+          setNewUnitTrustActionLabel(e.target.value);
+        }}
+        placeholder={'DD/MM/YYYY hh:mm'}
+      />
+
+      <label>Action Type</label>
+      <input
+        value={'new-ut'}
+        onChange={e => {
+          setNewUnitTrustActionLabel('new-ut');
+        }}
+        placeholder={'Name'}
+      />
+
+      <Button onClick={onCreate}>Create</Button>
+    </form>
   );
 };
 
-export default Trust;
+const UnitTrustActionUpdateDelete = ({ unitTrustAction }) => {
+  const [label, setLabel] = React.useState(unitTrustAction);
+
+  const onUpdate = () => {
+    const db = firebase.firestore();
+    db.collection('actions')
+      .doc(unitTrustAction.id)
+      .set({ label });
+  };
+
+  const onDelete = () => {
+    const db = firebase.firestore();
+    db.collection('actions')
+      .doc(unitTrustAction.id)
+      .delete();
+  };
+
+  return (
+    <div style={{ padding: '15px' }}>
+      <label>Name</label>
+      <input
+        value={label}
+        onChange={e => {
+          setLabel(e.target.value);
+        }}
+      />
+
+      <Button color="info" onClick={onUpdate}>
+        Update
+      </Button>
+      <Button style={{ margin: '15px' }} color="danger" onClick={onDelete}>
+        Delete
+      </Button>
+    </div>
+  );
+};
+
+const UnitTrust = () => {
+  //check permissions in firestore, read and write access granted?
+
+  const [UnitTrustActions, setUnitTrustActions] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const db = firebase.firestore();
+      const data = await db.collection('actions').get();
+      setUnitTrustActions(
+        data.docs.map(doc => ({ ...doc.data(), id: doc.id }))
+      );
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <Row>
+        <Button color="danger" id="toggler" style={{ marginBottom: '1rem' }}>
+          <strong>+</strong> Add New Unit Trust Insurance
+        </Button>
+        <UncontrolledCollapse toggler="#toggler">
+          <Card>
+            <CardBody>
+              <UnitTrustActionCreate />
+            </CardBody>
+          </Card>
+        </UncontrolledCollapse>
+      </Row>
+      <Row style={{ marginTop: 30 }}>
+        <div>
+          {UnitTrustActions.map(action => (
+            <div>
+              {action.actionType === 'new-ut' ? (
+                <Card>
+                  <span>{action.fundName}</span>
+                  <UnitTrustActionUpdateDelete
+                    unitTrustAction={action}
+                  ></UnitTrustActionUpdateDelete>
+                </Card>
+              ) : (
+                ''
+              )}
+            </div>
+          ))}
+        </div>
+      </Row>
+    </div>
+  );
+};
+
+export default UnitTrust;
